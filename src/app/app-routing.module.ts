028c1907153a7133ace7e85c6fbbb0949461517a
twerske <pokepsi@gmail.com>
2021-04-19 12:08:18 -0700

    codelab: define unique page titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,9 +21,9 @@ import { ShopComponent } from './shop/shop.component';
 
 // TODO: #4. Define unique page titles
 const routes: Routes = [
-  { path: 'shop', component: ShopComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'locate', component: LocationComponent },
+  { path: 'shop', component: ShopComponent, title: 'Our Shop – a11y in Angular' },
+  { path: 'about', component: AboutComponent, title: 'Our Story - a11y in Angular' },
+  { path: 'locate', component: LocationComponent, title: 'Find Us - a11y in Angular' },
   { path: '',   redirectTo: '/shop', pathMatch: 'full' },
   { path: '**', component: ShopComponent },
 ];
