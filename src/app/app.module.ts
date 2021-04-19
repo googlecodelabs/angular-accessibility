c02a4ee69a393d531ba04dfc267887450005a1f5
twerske <pokepsi@gmail.com>
2021-04-19 12:18:07 -0700

    codelab: add the power of @angular/cdk/a11y

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { ShopComponent } from './shop/shop.component';
 
 // TODO: #9. Add the power of @angular/cdk/a11y
+import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
   declarations: [
@@ -64,6 +65,7 @@ import { ShopComponent } from './shop/shop.component';
     MatSliderModule,
     MatToolbarModule,
     GoogleMapsModule,
+    A11yModule,
   ],
   providers: [Title],
   bootstrap: [AppComponent]
